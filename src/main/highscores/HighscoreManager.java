fc7ed636f691eca915cf918bf8eafeb8351535d8
Amy Lewis <amylewis1034@gmail.com>
2016-11-17 15:15:05 -0800

    Edited style

--- src/main/highscores/HighscoreManager.java
+++ src/main/highscores/HighscoreManager.java
@@ -28,6 +28,11 @@ public class HighscoreManager {
     scores = new ArrayList<Score>();
   }
 
+  /**
+   * Method to get and sort the high scores.
+   * 
+   * @return list of high scores
+   */
   public ArrayList<Score> getScores() {
     loadScoreFile();
     sort();
@@ -39,69 +44,88 @@ public class HighscoreManager {
     Collections.sort(scores, comparator);
   }
 
+  /**
+   * Method to add a new high score to the list.
+   * 
+   * @param name name of the user with the new high score
+   * @param score score of the user with the new high score
+   */
   public void addScore(String name, int score) {
     loadScoreFile();
     scores.add(new Score(name, score));
     updateScoreFile();
   }
 
+  /**
+   * Method to lead the file where the high scores are stored.
+   */
   @SuppressWarnings("unchecked")
   public void loadScoreFile() {
     try {
       inStream = new ObjectInputStream(new FileInputStream(HS_FILE));
       scores = (ArrayList<Score>) inStream.readObject();
-    } catch (ClassNotFoundException e) {
-      System.out.println("Class Not Found Error: " + e.getMessage());
-    } catch (FileNotFoundException e) {
-      System.out.println("File Not Found Error: " + e.getMessage());
-    } catch (IOException e) {
-      System.out.println("IO Error: " + e.getMessage());
+    } catch (ClassNotFoundException ex) {
+      System.out.println("Class Not Found Error: " + ex.getMessage());
+    } catch (FileNotFoundException ex) {
+      System.out.println("File Not Found Error: " + ex.getMessage());
+    } catch (IOException ex) {
+      System.out.println("IO Error: " + ex.getMessage());
     } finally {
       try {
         if (outStream != null) {
           outStream.flush();
           outStream.close();
         }
-      } catch (IOException e) {
-        System.out.println("IO Error: " + e.getMessage());
+      } catch (IOException ex) {
+        System.out.println("IO Error: " + ex.getMessage());
       }
     }
   }
 
+  /**
+   * Method to update the file where the high scores are stored with the new scores.
+   */
   public void updateScoreFile() {
     try {
       outStream = new ObjectOutputStream(new FileOutputStream(HS_FILE));
       outStream.writeObject(scores);
-    } catch (FileNotFoundException e) {
-      System.out.println("File Not Found Error: " + e.getMessage());
-    } catch (IOException e) {
-      System.out.println("IO Error: " + e.getMessage());
+    } catch (FileNotFoundException ex) {
+      System.out.println("File Not Found Error: " + ex.getMessage());
+    } catch (IOException ex) {
+      System.out.println("IO Error: " + ex.getMessage());
     } finally {
       try {
         if (outStream != null) {
           outStream.flush();
           outStream.close();
         }
-      } catch (IOException e) {
-        System.out.println("[Update] Error: " + e.getMessage());
+      } catch (IOException ex) {
+        System.out.println("[Update] Error: " + ex.getMessage());
       }
     }
   }
 
+  /**
+   * Method that returns a String of all of the high scores and the names of the users
+   * holding those scores.
+   * 
+   * @return String of users and high scores
+   */
   public String getHighscoreName() {
     String highscore = "";
     final int maxNumScores = 10;
 
-        ArrayList<Score> scores;
-        scores = getScores();
+    ArrayList<Score> scores;
+    scores = getScores();
 
-        int numScores = scores.size();
-        if (numScores > maxNumScores) {
-          numScores = maxNumScores;
-        }
-       for (int i = 0; i < numScores; i++) {
-            highscore += (i + 1) + ".\t" + scores.get(i).getScoreName() + "\t\t" + scores.get(i).getScore() + "\n";
-        }
-        return highscore;
+    int numScores = scores.size();
+    if (numScores > maxNumScores) {
+      numScores = maxNumScores;
+    }
+    for (int i = 0; i < numScores; i++) {
+      highscore +=
+          (i + 1) + ".\t" + scores.get(i).getScoreName() + "\t\t" + scores.get(i).getScore() + "\n";
+    }
+    return highscore;
   }
 }
