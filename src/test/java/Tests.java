a89318bd379d76bf2f205d0120b7f921f8881d19
Amy Lewis <amylewis1034@gmail.com>
2016-11-30 08:48:33 -0800

    even more tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.awt.Rectangle;
 import java.util.ArrayList;
@@ -17,6 +18,8 @@ import arcade.src.main.Enemy;
 import arcade.src.main.EnemyCollideObjects;
 import arcade.src.main.FriendlyCollideObjects;
 import arcade.src.main.Player;
+import arcade.src.main.Score;
+import arcade.src.main.ScoreComparator;
 import arcade.src.main.SpaceInvaders;
 
 public class Tests {
@@ -115,9 +118,22 @@ public class Tests {
     Player player = new Player(10, 10, siGame);
     double y1Vel = 0.0;
     
-    player.setYCoord(12);
+    player.setYVel(12);
 
     assertEquals((int) y1Vel, 0);
+  }
+  
+  @Test
+  public void rectangleBoundsPlayerTest() {
+    SpaceInvaders siGame = new SpaceInvaders();
+    Player player = new Player(10, 10, siGame);
+    int xCoord = (int) player.getXCoord();
+    int yCoord = (int) player.getYCoord();
+
+    Rectangle rect = new Rectangle(xCoord, yCoord, 10, 20);
+    Rectangle rect2 = player.getRectBounds(10, 20);
+
+    assertEquals(rect, rect2);
   }
 
   @Test
@@ -137,13 +153,23 @@ public class Tests {
   }
 
   @Test
-  public void collisionPhysicsTest() {
+  public void collisionPhysicsFalseTest() {
     SpaceInvaders siGame = new SpaceInvaders();
     Controller controller = new Controller(siGame);
     Bullet bullet = new Bullet(10, 10);
     Enemy enemy = new Enemy(100, 100, siGame, controller);
 
     assertFalse(CollisionPhysics.isCollision(bullet, enemy));
+  }
+  
+  @Test
+  public void collisionPhysicsTrueTest() {
+    SpaceInvaders siGame = new SpaceInvaders();
+    Controller controller = new Controller(siGame);
+    Bullet bullet = new Bullet(10, 10);
+    Enemy enemy = new Enemy(10, 10, siGame, controller);
+
+    assertTrue(CollisionPhysics.isCollision(bullet, enemy));
   }
 
   @Test
@@ -190,6 +216,34 @@ public class Tests {
 
     assertEquals(enemyList2.size(), 1);
   }
+  
+  @Test
+  public void getScoreTest() {
+    Score scor = new Score("Amy", 10);
+    int score = 10;
+    
+    assertEquals(scor.getScore(), score);
+  }
+  
+  @Test
+  public void getScoreNameTest() {
+    Score scor = new Score("Amy", 10);
+    String name = "Amy";
+    
+    assertEquals(scor.getScoreName(), name);
+  }
+  
+  @Test
+  public void scoreCompareTest() {
+    ScoreComparator sc = new ScoreComparator();
+    Score score1 = new Score("Amy", 10);
+    Score score2 = new Score("Hanna", 20);
+    
+    int compared = sc.compare(score1, score2);   
+    
+    assertEquals(compared, 1);
+  }
+  
   /*
    * @Test public void hsManagerSnakeTest() { HighscoreManagerSnake hsSnake = new
    * HighscoreManagerSnake(); ManageHighscores hsManage = hsSnake.getManageHighscores();
