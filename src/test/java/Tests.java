3fc6468f4892e114eef38592ceddb838b4fc6e06
Amy Lewis <amylewis1034@gmail.com>
2016-11-29 22:41:16 -0800

    more tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,7 +1,15 @@
 import static org.junit.Assert.assertEquals;
 
-import arcade.src.main.Bullet;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import arcade.src.main.ArcadeConcreteSubject;
+import arcade.src.main.ArcadeConcreteSubject.Arcade;
+import arcade.src.main.ArcadeObserver;
+import arcade.src.main.Bullet;
+import arcade.src.main.SpaceInvaders;
 
 public class Tests {
   
@@ -30,5 +38,31 @@ public class Tests {
     int ypSpeed = (int)bullet.getYCoord() - bulletSpeed;
     
     assertEquals(ypSpeed, 0);
+  }
+  
+  @Test
+  public void rectangleBoundsTest() {
+    Bullet bullet = new Bullet(10, 10);
+    int xCoord = (int)bullet.getXCoord();
+    int yCoord = (int)bullet.getYCoord();
+    
+    Rectangle rect = new Rectangle(xCoord, yCoord, 10, 20);
+    Rectangle rect2 = bullet.getRectBounds(10, 20);
+    
+    assertEquals(rect, rect2);
+  }
+  
+  @Test
+  public void observersTest() {
+    ArcadeConcreteSubject subject = new ArcadeConcreteSubject();
+    ArrayList<ArcadeObserver> observers = new ArrayList<ArcadeObserver>();
+    Arcade state = subject.getState();
+    ArcadeObserver siGame = new SpaceInvaders();
+    
+    subject.registerObservers(siGame);
+    subject.setState(Arcade.HIGHSCORES);
+    subject.unRegisterObservers(siGame);
+    
+    assertEquals(observers.size(), 0);
   }
 }
