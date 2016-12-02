c48fde29b70d87eab44ed664db7006b8f133220d
Amy Lewis <amylewis1034@gmail.com>
2016-12-01 19:12:30 -0800

    enemy tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,12 +2,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.awt.Rectangle;
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-import org.junit.Test;
-
 import arcade.src.main.ArcadeConcreteSubject;
 import arcade.src.main.ArcadeConcreteSubject.Arcade;
 import arcade.src.main.ArcadeObserver;
@@ -17,11 +11,19 @@ import arcade.src.main.Controller;
 import arcade.src.main.Enemy;
 import arcade.src.main.EnemyCollideObjects;
 import arcade.src.main.FriendlyCollideObjects;
+import arcade.src.main.HighscoreManagerSnake;
+import arcade.src.main.HighscoreManagerSpace;
 import arcade.src.main.HighscoreMenu;
+import arcade.src.main.ManageHighscores;
 import arcade.src.main.Player;
 import arcade.src.main.Score;
 import arcade.src.main.ScoreComparator;
 import arcade.src.main.SpaceInvaders;
+import org.junit.Test;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Tests {
 
@@ -51,14 +53,14 @@ public class Tests {
 
     assertEquals(ypSpeed, 0);
   }
-  
+
   @Test
   public void rectangleBoundsBulletTest() {
     Bullet bullet = new Bullet(10, 10);
-    int xCoord = (int) bullet.getXCoord();
-    int yCoord = (int) bullet.getYCoord();
+    int x1Coord = (int) bullet.getXCoord();
+    int y1Coord = (int) bullet.getYCoord();
 
-    Rectangle rect = new Rectangle(xCoord, yCoord, 10, 20);
+    Rectangle rect = new Rectangle(x1Coord, y1Coord, 10, 20);
     Rectangle rect2 = bullet.getRectBounds(10, 20);
 
     assertEquals(rect, rect2);
@@ -107,7 +109,7 @@ public class Tests {
     SpaceInvaders siGame = new SpaceInvaders();
     Player player = new Player(10, 10, siGame);
     double x1Vel = 0.0;
-    
+
     player.setXVel(12);
 
     assertEquals((int) x1Vel, 0);
@@ -118,29 +120,70 @@ public class Tests {
     SpaceInvaders siGame = new SpaceInvaders();
     Player player = new Player(10, 10, siGame);
     double y1Vel = 0.0;
-    
+
     player.setYVel(12);
 
     assertEquals((int) y1Vel, 0);
   }
-  
+
   @Test
   public void rectangleBoundsPlayerTest() {
     SpaceInvaders siGame = new SpaceInvaders();
     Player player = new Player(10, 10, siGame);
-    int xCoord = (int) player.getXCoord();
-    int yCoord = (int) player.getYCoord();
+    int x1Coord = (int) player.getXCoord();
+    int y1Coord = (int) player.getYCoord();
 
-    Rectangle rect = new Rectangle(xCoord, yCoord, 10, 20);
-    Rectangle rect2 = player.getRectBounds(10, 20);
+    Rectangle rect = new Rectangle(x1Coord, y1Coord, player.getWidth(),player.getHeight());
+    Rectangle rect2 = player.getRectBounds();
 
     assertEquals(rect, rect2);
+  }
+  
+  @Test
+  public void enemyWidthTest() {
+    SpaceInvaders siGame = new SpaceInvaders();
+    Controller controller = new Controller(siGame);
+    Enemy enemy = new Enemy(10, 10, siGame, controller);
+    int enemyWidth = 80;
+    int width = enemy.getWidth();
+
+    assertEquals(width, enemyWidth);
+  }
+
+  @Test
+  public void enemyHeightTest() {
+    SpaceInvaders siGame = new SpaceInvaders();
+    Controller controller = new Controller(siGame);
+    Enemy enemy = new Enemy(10, 10, siGame, controller);
+    int enemyHeight = 45;
+    int width = enemy.getHeight();
+
+    assertEquals(width, enemyHeight);
+  }
+
+  @Test
+  public void enemyXCoordTest() {
+    SpaceInvaders siGame = new SpaceInvaders();
+    Controller controller = new Controller(siGame);
+    Enemy enemy = new Enemy(10, 10, siGame, controller);
+    int x = (int) enemy.getXCoord();
+    
+    assertEquals((int) enemy.getXCoord(), x);
+  }
+
+  @Test
+  public void enemyYCoordTest() {
+    SpaceInvaders siGame = new SpaceInvaders();
+    Controller controller = new Controller(siGame);
+    Enemy enemy = new Enemy(10, 10, siGame, controller);
+    int y = (int) enemy.getYCoord();
+
+    assertEquals((int) enemy.getYCoord(), y);
   }
 
   @Test
   public void observersRegisterTest() {
     ArcadeConcreteSubject subject = new ArcadeConcreteSubject();
-    ArrayList<ArcadeObserver> observers = new ArrayList<ArcadeObserver>();
     Arcade state = subject.getState();
     ArcadeObserver siGame = new SpaceInvaders();
 
@@ -149,19 +192,22 @@ public class Tests {
     if (state == Arcade.HIGHSCORES) {
       subject.unRegisterObservers(siGame);
     }
+    
+    ArrayList<ArcadeObserver> observers = new ArrayList<ArcadeObserver>();
 
     assertEquals(observers.size(), 0);
   }
-  
+
   @Test
   public void observersUnregisterTest() {
     ArcadeConcreteSubject subject = new ArcadeConcreteSubject();
-    ArrayList<ArcadeObserver> observers = new ArrayList<ArcadeObserver>();
     HighscoreMenu hsMenu = new HighscoreMenu(subject);
 
     subject.registerObservers(hsMenu);
     subject.setState(Arcade.HIGHSCORES);
     subject.unRegisterObservers(hsMenu);
+
+    ArrayList<ArcadeObserver> observers = new ArrayList<ArcadeObserver>();
     
     assertEquals(observers.size(), 0);
   }
@@ -175,7 +221,7 @@ public class Tests {
 
     assertFalse(CollisionPhysics.isCollision(bullet, enemy));
   }
-  
+
   @Test
   public void collisionPhysicsTrueTest() {
     SpaceInvaders siGame = new SpaceInvaders();
@@ -230,73 +276,71 @@ public class Tests {
 
     assertEquals(enemyList2.size(), 1);
   }
-  
+
   @Test
   public void getScoreTest() {
     Score scor = new Score("Amy", 10);
     int score = 10;
-    
+
     assertEquals(scor.getScore(), score);
   }
-  
+
   @Test
   public void getScoreNameTest() {
     Score scor = new Score("Amy", 10);
     String name = "Amy";
-    
+
     assertEquals(scor.getScoreName(), name);
   }
-  
+
   @Test
   public void scoreCompare1Test() {
     ScoreComparator sc = new ScoreComparator();
     Score score1 = new Score("Amy", 10);
     Score score2 = new Score("Hanna", 20);
-    
-    int compared = sc.compare(score1, score2);   
-    
+
+    int compared = sc.compare(score1, score2);
+
     assertEquals(compared, 1);
   }
-  
+
   @Test
   public void scoreCompare2Test() {
     ScoreComparator sc = new ScoreComparator();
     Score score1 = new Score("Amy", 10);
     Score score2 = new Score("Hanna", 10);
-    
-    int compared = sc.compare(score1, score2);   
-    
+
+    int compared = sc.compare(score1, score2);
+
     assertEquals(compared, 0);
   }
-  
+
   @Test
   public void scoreCompare3Test() {
     ScoreComparator sc = new ScoreComparator();
     Score score1 = new Score("Amy", 10);
     Score score2 = new Score("Hanna", 1);
-    
-    int compared = sc.compare(score1, score2);   
-    
+
+    int compared = sc.compare(score1, score2);
+
     assertEquals(compared, -1);
   }
-  
-  /*
-   * @Test public void hsManagerSnakeTest() { HighscoreManagerSnake hsSnake = new
-   * HighscoreManagerSnake(); ManageHighscores hsManage = hsSnake.getManageHighscores();
-   * 
-   * hsManage.loadScoreFile(); ArrayList<Score> scores = hsManage.getHighscores(); ScoreComparator
-   * comparator = new ScoreComparator(); scores.sort(comparator); hsManage.addScore("Amy", 0);
-   * hsManage.sort(); hsManage.updateScoreFile();
-   * 
-   * assertEquals(hsManage.getHighscoreName().size(), 5); }
-   * 
-   * @Test public void hsManagerSpaceTest() { HighscoreManagerSpace hsSpace = new
-   * HighscoreManagerSpace(); ManageHighscores hsManage = hsSpace.getManageHighscores();
-   * 
-   * hsManage.loadScoreFile(); ArrayList<Score> scores = hsManage.getHighscores(); ScoreComparator
-   * comparator = new ScoreComparator(); scores.sort(comparator); hsManage.addScore("Amy", 0);
-   * hsManage.sort(); hsManage.updateScoreFile();
-   * 
-   * assertEquals(hsManage.getHighscoreName().size(), 5); }
-   */
+
+
+  @Test
+  public void hsManagerSnakeTest() {
+    HighscoreManagerSnake hsSnake = new HighscoreManagerSnake();
+    ManageHighscores hsManage = hsSnake.getManageHighscores();
+
+    assertEquals(hsManage.getHighscoreName().size(), 5);
+  }
+
+  @Test
+  public void hsManagerSpaceTest() {
+    HighscoreManagerSpace hsSpace = new HighscoreManagerSpace();
+    ManageHighscores hsManage = hsSpace.getManageHighscores();
+
+    assertEquals(hsManage.getHighscoreName().size(), 5);
+  }
+
 }
